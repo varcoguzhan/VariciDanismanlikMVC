7423804270983a69a3db99e92a2c4bbc82434245
varcoguzhan <129661142+varcoguzhan@users.noreply.github.com>
2023-11-23 23:14:57 +0300

    son
    son

--- Controllers/IletisimController.cs
+++ Controllers/IletisimController.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using VariciDanismanlikMVC.Data;
 
 namespace VariciDanismanlikMVC.Controllers
 {
     public class IletisimController: Controller
     {
+        private readonly IletisimContext _context;
+        public IletisimController(IletisimContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Contact(Iletisim model)
+        {
+            _context.Iletisimler.Add(model);
+            _context.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
+        }
     }
+
+ 
 }
